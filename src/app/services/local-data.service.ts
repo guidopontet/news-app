12cf25a4d3021ea412748db61855dc2ef9d2e231
Guido Pontet <guidopontet@gmail.com>
2019-08-10 14:52:41 -0300

    Add toast messages

--- src/app/services/local-data.service.ts
+++ src/app/services/local-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Article } from '../interfaces/interfaces';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,18 @@ export class LocalDataService {
   public favorites: Article[] = [];
 
   constructor(
-    private storage: Storage
+    private storage: Storage,
+    private toastController: ToastController
   ) {
     this.loadFavorites();
+  }
+
+  async presentToast(message: string, duration = 2000) {
+    const toast = await this.toastController.create({
+      message,
+      duration
+    });
+    toast.present();
   }
 
   saveToFavorites(article: Article) {
@@ -22,11 +32,14 @@ export class LocalDataService {
       this.favorites.unshift(article);
       this.storage.set('favorites', this.favorites);
     }
+
+    this.presentToast('Agregado a favoritos');
   }
 
   deleteFromFavorites(article: Article) {
     this.favorites = this.favorites.filter(art => art !== article);
     this.storage.set('favorites', this.favorites);
+    this.presentToast('Eliminado a favoritos');
   }
 
   async loadFavorites() {
